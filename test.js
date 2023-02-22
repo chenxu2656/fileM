6e7fe775a5b23e2f9eeadfe9f2dbb7da61375c02
chenchen.xu <chenxu2656@163.com>
2023-02-22 21:27:01 +0800

    update

--- test.js
+++
@@ -1,6 +0,0 @@
-const randomVerificationCode = ()=>{
-    const randomString =  Math.random().toString(10).slice(2,8);
-    console.log(randomString);
-    return randomString
-}
-randomVerificationCode()
